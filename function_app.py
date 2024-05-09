eba60b29d8ce3349c1fa1f3f6cacfb7799e43aa8
ubah <ivan.dvylyuk@gmail.com>
2024-05-09 21:31:39 +0300

    refactoring code

--- function_app.py
+++ function_app.py
@@ -2,17 +2,20 @@ import azure.functions as func
 import logging
 import os
 from azure.storage.fileshare import ShareFileClient
-from azure.storage.blob import BlobServiceClient, BlobClient, ContentSettings
+from azure.storage.blob import BlobServiceClient, ContentSettings
+from storage_handler import StorageHandler
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
+file_share_name = "fsbestrongpdf"
+container_name = "bestrong-data"
+storage_handler = StorageHandler(file_share_name, container_name)
 
-connection_string = os.getenv("STORAGE_ACCOUNT_CONNECTION_STRING")
+
 
 @app.route(route="http_trigger")
 def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
-
     pdf_file_name = req.params.get('file_name')
 
     if not pdf_file_name:
@@ -22,46 +25,11 @@ def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
         )
 
     try:
-        file_share_name = "fsbestrongpdf"
-        file_client = ShareFileClient.from_connection_string(
-            conn_str=connection_string,
-            share_name=file_share_name,  
-            file_path=pdf_file_name
-        )
+        storage_handler.download_file_from_share(file_share_name, pdf_file_name)
 
-        with open(pdf_file_name, "wb") as file:
-            download = file_client.download_file()
-            file.write(download.readall())
-
-        blob_service_client = BlobServiceClient.from_connection_string(connection_string)
-        container_name = "bestrong-data"
-        container_client = blob_service_client.get_container_client(container_name)
-
-
-        # blob_client = blob_service_client.get_blob_client(container=container_name, blob=pdf_file_name)
-        with open(pdf_file_name, "rb") as file:
-            container_client.upload_blob(pdf_file_name, 
-                                         file, 
-                                         overwrite=True,
-                                         content_settings=ContentSettings(content_type="application/pdf"))
-            # container_client.upload_blob(file, 
-            #                         overwrite=True, 
-            #                         content_settings=ContentSettings(content_type="application/pdf"))
-
-        
-
+        # storage_handler.upload_file_to_blob(pdf_file_name)
+        storage_handler.delete_local_file(pdf_file_name)
         return func.HttpResponse("PDF file uploaded to Azure Blob Storage successfully.", status_code=200)
-
-
-        # with open(pdf_file_name, "wb") as file:
-        #     download = file_client.download_file()
-        #     file.write(download.readall())
-
-        # with open(pdf_file_name, "rb") as file:
-        #     pdf_content = file.read()
-        #     return func.HttpResponse(pdf_content, mimetype="application/pdf")
-        
-        os.remove(pdf_file_name)
 
     except Exception as e:
         logging.error(f"An error occurred: {str(e)}")
@@ -69,3 +37,52 @@ def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
             f"An error occurred while processing the request: {str(e)}",
             status_code=500
         )
+
+    # try:
+    #     file_share_name = "fsbestrongpdf"
+    #     file_client = ShareFileClient.from_connection_string(
+    #         conn_str=connection_string,
+    #         share_name=file_share_name,  
+    #         file_path=pdf_file_name
+    #     )
+
+    #     with open(pdf_file_name, "wb") as file:
+    #         download = file_client.download_file()
+    #         file.write(download.readall())
+
+    #     blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+    #     container_name = "bestrong-data"
+    #     container_client = blob_service_client.get_container_client(container_name)
+
+
+    #     # blob_client = blob_service_client.get_blob_client(container=container_name, blob=pdf_file_name)
+    #     with open(pdf_file_name, "rb") as file:
+    #         container_client.upload_blob(pdf_file_name, 
+    #                                      file, 
+    #                                      overwrite=True,
+    #                                      content_settings=ContentSettings(content_type="application/pdf"))
+    #         # container_client.upload_blob(file, 
+    #         #                         overwrite=True, 
+    #         #                         content_settings=ContentSettings(content_type="application/pdf"))
+
+        
+
+    #     return func.HttpResponse("PDF file uploaded to Azure Blob Storage successfully.", status_code=200)
+
+
+    #     # with open(pdf_file_name, "wb") as file:
+    #     #     download = file_client.download_file()
+    #     #     file.write(download.readall())
+
+    #     # with open(pdf_file_name, "rb") as file:
+    #     #     pdf_content = file.read()
+    #     #     return func.HttpResponse(pdf_content, mimetype="application/pdf")
+        
+    #     os.remove(pdf_file_name)
+
+    # except Exception as e:
+    #     logging.error(f"An error occurred: {str(e)}")
+    #     return func.HttpResponse(
+    #         f"An error occurred while processing the request: {str(e)}",
+    #         status_code=500
+    #     )
---
+++ storage_handler.py
@@ -0,0 +1,37 @@
+import os
+from azure.storage.fileshare import ShareFileClient
+from azure.storage.blob import BlobServiceClient, ContentSettings
+
+class StorageHandler:
+    def __init__(self, file_share_name, container_name):
+        self.file_share_name = file_share_name
+        self.container_name = container_name
+        self.connection_string = os.getenv("STORAGE_ACCOUNT_CONNECTION_STRING")
+        self.blob_service_client = BlobServiceClient.from_connection_string(self.connection_string)
+
+    def download_file_from_share(self, file_share_name, file_name):
+        file_client = ShareFileClient.from_connection_string(
+            conn_str=self.connection_string,
+            share_name=file_share_name,
+            file_path=file_name
+        )
+
+        with open(file_name, "wb") as file:
+            download = file_client.download_file()
+            file.write(download.readall())
+        return 
+
+    # def upload_file_to_blob(self, file_name):
+       
+    #     container_client = self.blob_service_client.get_container_client(self.container_name)
+
+    #     with open(file_name, "rb") as file:
+    #         container_client.upload_blob(
+    #             name=file_name,
+    #             data=file,
+    #             overwrite=True,
+    #             content_settings=ContentSettings(content_type="application/pdf")
+    #         )
+
+    def delete_local_file(self, file_name):
+        os.remove(file_name)
